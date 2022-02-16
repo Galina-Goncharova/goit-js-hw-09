c78e1c1e25387e656b84235bc40f8fc8569469fb
Galina-Goncharova <galinagoncharova1112@gmail.com>
2022-02-16 21:28:14 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
 
+    const startBtn = document.querySelector('button[data-start]');
+    const stopBtn = document.querySelector('button[data-stop]');
+    const body = document.querySelector('body');
+
+    let timerId = null;
+    function getRandomHexColor() {
+      return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+
+   startBtn.addEventListener("click", () => {
+    if (startBtn.disabled) {
+        return;
+    }
+      timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+    startBtn.setAttribute("disabled", true);
+    });
+
+
+    stopBtn.addEventListener("click", () => {
+      clearInterval(timerId);
+        startBtn.removeAttribute("disabled");
+    });
